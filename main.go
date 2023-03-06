132b5b0b33f5bb4191e4fb21803d004c5c13458e
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-06 06:56:54 -0500

    photo src struct added

--- main.go
+++ main.go
@@ -25,11 +25,31 @@ func NewClient(token string) *Client {
 }
 
 type SearchResult struct {
-	Page         int32
-	PerPage      int32
-	TotalResults int32
-	NextPage     string
-	Photos       []Photo
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_Results"`
+	NextPage     string  `json:"next_page"`
+	Photos       []Photo `json:"photos"`
+}
+type Photo struct {
+	Id              int32       `json:"id"`
+	Width           int32       `json:"width"`
+	Height          int32       `json:"height"`
+	Url             string      `json:"url"`
+	Photographer    string      `json:"photographer"`
+	PhotographerUrl string      `json:"photographer_url"`
+	Src             PhotoSource `json:"src"`
+}
+
+type PhotoSource struct {
+	Original  string `json:"original"`
+	Large     string `json:"large"`
+	Large2x   string `json:"large2x"`
+	Medium    string `json:"medium"`
+	Potrait   string `json:"potrait"`
+	Squire    string `json:"squire"`
+	Landscape string `json:"landscape"`
+	Tiny      string `json:"tiny"`
 }
 
 func main() {
