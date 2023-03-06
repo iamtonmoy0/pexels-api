11e3c5cfc4c9d4901613ca209ec8e58bddbf9fa4
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-06 04:19:38 -0500

    token imported and passed

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // api link
 const (
 	PhotoApi = "https://api.pexels.com/v1"
@@ -7,5 +12,15 @@ const (
 )
 
 func main() {
-
+	os.Setenv("PexelsToken", "OLY1UXu7nWNqhhiV5XXXTcU8SHJPaMUEWzotNouYLKhqNuTyLsnXjgxS")
+	var TOKEN = os.Getenv("PexelsToken")
+	var c = NewClient(TOKEN)
+	result, err := c.SearchPhotos("wave")
+	if err != nil {
+		fmt.Errorf("Search error:%v", err)
+	}
+	if result.Page == 0 {
+		fmt.Errorf("wrong search result")
+	}
+	fmt.Println(result)
 }
