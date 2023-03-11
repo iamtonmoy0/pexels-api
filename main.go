a94e6fb6c4ea8825a7d78767f3e99a085ed03684
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-11 08:16:15 -0500

    error fixed

--- main.go
+++ main.go
@@ -113,7 +113,25 @@ func (c *Client) SearchPhotos(query string, perPage, page int) (*SearchResult, e
 	err = json.Unmarshal(data, &result)
 	return &result, err
 }
-func (c *Client) requestDoWithAuth(method, url string) (*http.Response, err) {
+func (c *Client) CuratedPhotos(perPage, page int) (*CuratedResult, error) {
+	url := fmt.Sprintf(PhotoApi+"/curated?per_page=%d&page=%d", perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result CuratedResult
+	err = json.Unmarshal(data, &result)
+	return &result, err
+}
+
+func (c *Client) requestDoWithAuth(method, url string) (*http.Response, error) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, err
@@ -158,25 +176,65 @@ func (c *Client) GetRandomPhoto() (*Photo, error) {
 }
 
 func (c *Client) SearchVideo(query, perPage, page int) (*VideoSearchReasult, error) {
+	url := fmt.Sprintf(VideoApi+"/search?query=%s&per_page=%d&page=%d", query, perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result VideoSearchResult
+	err = json.Unmarshal(data, &result)
+	return &result, err
 
 }
 func (c *Client) PopularVideo(perPage, page int) (*PopularVideos, error) {
+	url := fmt.Sprintf(VideoApi+"/popular?per_page=%d&page=%d", perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result PopularVideos
+	err = json.Unmarshal(data, &result)
+	return &result, err
 
 }
 func (c *Client) GetRandomVideo() (*Video, error) {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(1001)
+	result, err := c.PopularVideo(1, randNum)
+	if err == nil && len(result.Videos) == 1 {
+		return &result.Videos[0], nil
+	}
+	return nil, err
 
+}
+func (c *Client) GetRemainingRequestsInThisMonth() int32 {
+	return c.RemainingTimes
 }
 
 func main() {
 	os.Setenv("PexelsToken", "OLY1UXu7nWNqhhiV5XXXTcU8SHJPaMUEWzotNouYLKhqNuTyLsnXjgxS")
 	var TOKEN = os.Getenv("PexelsToken")
 	var c = NewClient(TOKEN)
-	result, err := c.SearchPhotos("wave")
+	result, err := c.GetRandomVideo("wave")
 	if err != nil {
 		fmt.Errorf("Search error:%v", err)
 	}
-	if result.Page == 0 {
-		fmt.Errorf("wrong search result")
-	}
+	// if result.Page == 0 {
+	// 	fmt.Errorf("wrong search result")
+	// }
 	fmt.Println(result)
 }
